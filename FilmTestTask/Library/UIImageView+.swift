d39193e90d346fc6389c36de2f2d7f40d47d9a89
Anatolii <tasamayapochta@rambler.ru>
2023-07-02 21:43:21 +0300

    work with network layer

--- FilmTestTask/Library/UIImageView+.swift
+++ FilmTestTask/Library/UIImageView+.swift
@@ -10,18 +10,15 @@ import UIKit
 extension UIImageView {
     
     func loadImage(url: String) {
-        let network = NetworkManager()
-        network.getPhoto(url: url) { result in
-            switch result {
-            case .success(let success):
-                guard let poster = UIImage(data: success) else {
-                    return }
-                DispatchQueue.main.async {
-                    self.image = poster
-                }
-            case .failure(_):
-                DispatchQueue.main.async {
-                    self.image = UIImage()
+        guard let url1 = URL(string: url) else {
+            return
+        }
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url1) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.image = image
+                    }
                 }
             }
         }

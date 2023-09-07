0f8a3a1f6040a7f0a85952b36b7d17dea263415c
Anatolii <tasamayapochta@rambler.ru>
2023-09-07 22:50:58 +0300

    added network monitoring

---
+++ FilmTestTask/Services/NetworMonitor/NetworkMonitorManager.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkMonitorManager.swift
+//  FilmTestTask
+//
+//  Created by APPLE on 07.09.2023.
+//
+
+import Foundation
+import Network
+
+protocol NetworkMonitorProtocol {
+    
+    func startMonitoring()
+    func stopMonitoring()
+}
+
+final class NetworkMonitor: NetworkMonitorProtocol {
+    
+    func startMonitoring() {
+        
+    }
+    
+    func stopMonitoring() {
+        
+    }
+}
